70a18853c6838545f4a05a3fcffed7dc45639529
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-12-12 14:38:13 -0800

    add some methods to the datasource interface

--- datasources/datasouce.py
+++ datasources/datasouce.py
@@ -1,10 +1,29 @@
 # This file contains the interface thatdatasources should implement in order to be able to be swapped out
 # See https://realpython.com/python-interface/ for info
+from helpers import Status
+
 class DataSourceInterface:
-    def load_data_source(self, path: str, file_name: str) -> str:
-        """Load in the file for extracting text."""
+
+    def setup(self):
+        raise NotImplementedError()
+    
+    def get_status(self) -> Status:
+        raise NotImplementedError() 
+
+    def has_unread_data(self) -> bool:
+        raise NotImplementedError()
+
+    # def flush_buffer(self) -> bool:
+    #     raise NotImplementedError() 
+
+    def get_data_point(self):
         pass
 
-    # def extract_text(self, full_file_name: str) -> dict:
-    #     """Extract text from the currently loaded file."""
-    #     pass
+    def run(self):
+        raise NotImplementedError() 
+
+    def get_status(self):
+		return self.status
+
+	def get_buffer_size(self):
+		return len(self.buffer)
