b422e9ea30752f23dbbba3ac2b60c92da99092c4
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-12-12 15:03:27 -0800

    add status class for keeping track of status

---
+++ helpers/status.py
@@ -0,0 +1,20 @@
+from enum import Enum
+class State(Enum):
+    ERROR = 0
+    WAITING = 1
+    READY = 2
+    RECORDING = 3
+
+class Status():
+	state = State.WAITING
+	message = ""
+
+	def __init__(self, state, message):
+		self.state = state
+		self.message = message
+	
+	def get_state(self):
+		return self.state
+
+	def get_message(self):
+		return self.message
