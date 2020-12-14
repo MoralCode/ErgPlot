4c02f13378eb166f446b769ea14bc443ea9bf664
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-12-13 16:07:23 -0800

    reimplement lastnlines function with a queue

--- helpers/common.py
+++ helpers/common.py
@@ -3,56 +3,14 @@
 # Using OS module and buffering policy 
 		
 # importing os module 
-import os 
+# import os 
+from collections import deque
+from io import StringIO
 
 # Function to read 
 # last N lines of the file 
-def LastNlines(fname, N): 
-	# taking buffer size of 8192 bytes 
-	bufsize = 8192
+def LastNlines(fname, n): 
+	with open(fname, 'r') as f:
+		q = deque(f, n)  # replace 2 with n (lines read at the end)
+		return list(q)
 	
-	# calculating size of 
-	# file in bytes 
-	fsize = os.stat(fname).st_size 
-	
-	iter = 0
-	
-	# opening file using with() method 
-	# so that file get closed 
-	# after completing work 
-	with open(fname) as f: 
-		if bufsize > fsize: 
-				
-			# adjusting buffer size 
-			# according to size 
-			# of file 
-			bufsize = fsize-1
-			
-			# list to store  
-			# last N lines 
-			fetched_lines = [] 
-			
-			# while loop to 
-			# fetch last N lines 
-			while True: 
-				iter += 1
-				
-				# moving cursor to 
-				# the last Nth line 
-				# of file 
-				f.seek(fsize-bufsize * iter) 
-				
-				# storing each line 
-				# in list upto 
-				# end of file 
-				fetched_lines.extend(f.readlines()) 
-				
-				# halting the program 
-				# when size of list 
-				# is equal or greater to 
-				# the number of lines requested or 
-				# when we reach end of file 
-				if len(fetched_lines) >= N or f.tell() == 0: 
-						# print(''.join(fetched_lines[-N:])) 
-						return [line.strip() for line in fetched_lines]
-						# break
