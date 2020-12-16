43f5636d90601ab2697ff998524a9324c7d36745
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-12-16 15:02:03 -0800

    add test script to help determine data rates quickly

---
+++ misc/test.py
@@ -0,0 +1,26 @@
+import time
+from datasources.concept2 import Concept2
+
+data_source = Concept2()
+
+
+data_source.setup()
+
+def test_data_rate(function):
+	last_val = function()
+	update_time_millis = 0
+
+	while True:
+		val = function()
+		if val != last_val:
+			current_millis = int(round(time.time() * 1000))
+			delta = current_millis - update_time_millis
+			update_time_millis = current_millis
+			last_val = val
+			print("Updated Value after " + str(delta) + " milliseconds: " + str(val))
+		time.sleep(0.01)
+
+
+test_data_rate(data_source.get_monitor_data)
+
+
