69b34853b673ff0ed02898be0c88873e3dff5a8f
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-12-17 11:58:34 -0800

    set up plotting to show RPM calculated a few different ways

--- liveplot.py
+++ liveplot.py
@@ -12,51 +12,73 @@ style.use('fivethirtyeight')
 buffer_size = 10
 output_filename = "10k.csv"
 
+from datasources.concept2 import Concept2
 
-fig, axs = plt.subplots(2, 2)
-fig.suptitle('Grid of subplots')
+data_source = Concept2()
+
+
+data_source.setup()
+
+# setup the figure
+fig = plt.figure(1)
+plt.suptitle('Previous %d datapoints'%buffer_size, fontsize=12)
+axs = plt.gca()
 
 
 # setup the figure
 
+buffer = []
 
 def process_data_for_plots(i):
 
 	lines = []
 	try:
-		lines = LastNlines(output_filename, buffer_size)
+		# lines = LastNlines(output_filename, buffer_size)
 
 		data = []
 		forces = []
 
 		# clear all the axes
-		for row in range(len(axs)):
-			for col in range(len(axs)):
-				axs[row,col].clear()
-				axs[0,0].set_title('Impulse')
-				axs[0,1].set_title('Force')
-				axs[1,0].set_title('RPM')
-				axs[1,1].set_title('Work')
+		plt.cla()
+		# for row in range(len(axs)):
+		# 	for col in range(len(axs)):
+		# 		axs[row,col].clear()
+		# 		axs[0,0].set_title('Impulse')
+		# 		axs[0,1].set_title('Force')
+		# 		axs[1,0].set_title('RPM')
+		# 		axs[1,1].set_title('Work')
 
-		for line in lines:
-			time, dist, spm, pace, sdist, drivetime, _, _, _, _, impulse, _, work, force = line.split(",")
-			try:
-				fdata = [int(f) for f in force.split(";")]
-				data.append((float(time), float(dist), int(spm), float(pace), float(sdist), float(drivetime), int(impulse), int(work), fdata))
-				forces.extend(fdata)
-			except Exception:
-				data.append(tuple(line))
-	
+		# for line in lines:
+		# 	time, dist, spm, pace, sdist, drivetime, _, _, _, _, impulse, _, work, force = line.split(",")
+		# 	try:
+		# 		fdata = [int(f) for f in force.split(";")]
+		# 		data.append((float(time), float(dist), int(spm), float(pace), float(sdist), float(drivetime), int(impulse), int(work), fdata))
+		# 		forces.extend(fdata)
+		# 	except Exception:
+		# 		data.append(tuple(line))
+
+		pace_val = get_pace_values()
+		# print(pace_val)
+
+		buffer.append(pace_val)
 		
-		axs[0,0].plot( [item[6] for item in data],  label="impulse") # 'rs', ms=6
-		axs[0,1].plot( forces, label="force")
-		axs[1,0].plot( [convert_to_rpm(item[4], item[5]) for item in data], label="rpm")
-		axs[1,0].plot( [item[4]/item[5] for item in data], label="dist/stroke")
+		axs.plot( [point["raw"] for point in buffer], label="raw")
+		axs.plot( [point["differential"] for point in buffer], label="diff_real")
+		axs.plot( [point["differential2"] for point in buffer], label="diff_erg")
+		axs.plot( [point["stroke"] for point in buffer], label="stroke")
+		# 3 different ways
+
+
+
+		# axs[0,0].plot( [item[6] for item in data],  label="impulse") # 'rs', ms=6
+		# axs[0,1].plot( forces, label="force")
+		# axs[1,0].plot( [convert_to_rpm(item[4], item[5]) for item in data], label="rpm")
+		# axs[1,0].plot( [item[4]/item[5] for item in data], label="dist/stroke")
 
 		# plt.legend(loc='upper right')
-		axs[1,1].plot( [item[7] for item in data], label="Work")
+		# axs[1,1].plot( [item[7] for item in data], label="Work")
 	
-		# plt.legend(loc='upper right')
+		plt.legend(loc='upper right')
 
 		plt.draw()
 		plt.pause(0.01)
@@ -68,5 +90,61 @@ def convert_to_rpm(pace):
 	speed = 1/(pace/500)
 	return (speed*60*12.93)/3
 
-ani = animation.FuncAnimation(fig, process_data_for_plots, interval=1000)
+
+last_dist = 0
+last_duration = 0
+timestamp = int(round(time.time() * 1000))
+
+def get_differential_pace():
+	global last_duration, last_dist, timestamp
+	mon = data_source.get_monitor_data()
+
+	delta_x = delta_t = delta_t2 = 0
+
+	if mon["distance"] != last_dist or mon["time"] != last_duration:
+		current_millis = int(round(time.time() * 1000))
+		delta_t = (current_millis - timestamp)/1000
+		delta_t2 = (mon["time"] - last_duration)/100
+		delta_x = (mon["distance"] - last_dist)/10
+		timestamp = current_millis
+		last_dist = mon["distance"]
+		last_duration = mon["time"]
+	# substitute delta_t2 here for the erg-distance-based delta 
+		# print(delta_x)
+		# print(delta_t)
+		# if time == "actual":
+		return (delta_x/delta_t, delta_x/delta_t2)
+	return 0
+
+
+
+
+def get_stroke_pace():
+	sp = data_source.get_stroke_stats()
+	totaltime = (sp["drivetime"]+sp["recoverytime"])/100
+	return (sp["distance"]/100)/totaltime
+
+def get_pace_values():
+	# # all of these should be in meters per second
+	# point = {
+	# 	"raw": convert_to_rpm(1/(data_source.get_raw_pace()/500)),
+	# 	"differential": convert_to_rpm(get_differential_pace()),
+	# 	"stroke": convert_to_rpm(get_stroke_pace())
+	# }
+	diff_real, diff_erg=get_differential_pace()
+	point = {
+		"raw": 1/(data_source.get_raw_pace()/500),
+		"differential": diff_real,
+		"differential2": diff_erg,
+		"stroke": get_stroke_pace()
+	}
+	print(point)
+	return point
+
+
+
+ani = animation.FuncAnimation(fig, process_data_for_plots, interval=40)
+
 plt.show()
+print(buffer)
+# print(get_pace_values())
