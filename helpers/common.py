dd2ef7ae71a9c1842c6a5c6c4632350a993ce96c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-12-21 10:17:14 -0800

    add error checking to differential helper

--- helpers/common.py
+++ helpers/common.py
@@ -18,7 +18,23 @@ def separate_values(data, separator=","):
 	return separator.join([str(value) for value in data])
 
 def calculate_differential(start_val, start_time, end_val, end_time):
-	return (end_val-start_val)/((end_time-start_time)/1000)
+	# print(start_val, start_time, end_val, end_time)
+	try: 
+		delta_x = (float(end_val)-float(start_val))/1000
+		delta_t = ((float(end_time)-float(start_time))/1000)
+		if end_time == start_time:
+			
+			return delta_x/1
+		
+		# print(float(end_val))
+		# exit(0)
+
+		return round(delta_x, 8)/round(delta_t, 8)
+	
+	except (TypeError, ValueError, ZeroDivisionError) as e:
+		print("error: ")
+		print(e)
+		return 0
 
 def convert_to_rpm(speed):
 	# speed = 1/(pace/500)
